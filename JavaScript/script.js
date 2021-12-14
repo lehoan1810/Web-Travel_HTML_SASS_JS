b70168605b125aeb7069a4eed218f159a297caa6
puimekster <hoanhao18102000@gmail.com>
2021-12-14 16:42:37 +0700

    complete

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -44,3 +44,48 @@ var swiper = new Swiper(".home-slider", {
 		prevEl: ".swiper-button-prev",
 	},
 });
+
+var swiper = new Swiper(".product-slider", {
+	loop: true,
+	grabCursor: true,
+	spaceBetween: 20,
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+		},
+		640: {
+			slidesPerView: 2,
+		},
+		768: {
+			slidesPerView: 3,
+		},
+		1024: {
+			slidesPerView: 4,
+		},
+	},
+});
+
+var swiper = new Swiper(".review-slider", {
+	loop: true,
+	grabCursor: true,
+	spaceBetween: 20,
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+		},
+		640: {
+			slidesPerView: 2,
+		},
+		768: {
+			slidesPerView: 3,
+		},
+	},
+});
